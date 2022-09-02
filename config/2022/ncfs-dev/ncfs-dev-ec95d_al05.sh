c0777eb7a8bba94c1812a7822af5c5365d3763bf
BrianOBlanton <Brian_Blanton@Renci.Org>
2022-09-02 11:05:36 -0400

    added ec95d al05 config

---
+++ config/2022/ncfs-dev/ncfs-dev-ec95d_al05.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+#-------------------------------------------------------------------
+# config.sh: This file is read at the beginning of the execution of the ASGS to
+# set up the runs  that follow. It is reread at the beginning of every cycle,
+# every time it polls the datasource for a new advisory. This gives the user
+# the opportunity to edit this file mid-storm to change config parameters
+# (e.g., the name of the queue to submit to, the addresses on the mailing list,
+# etc)
+#-------------------------------------------------------------------
+#
+# Copyright(C) 2019 Jason Fleming
+#
+# This file is part of the ADCIRC Surge Guidance System (ASGS).
+#
+# The ASGS is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# ASGS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# the ASGS.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------
+
+# Fundamental
+
+INSTANCENAME=ec95d-al05-bob      # "name" of this ASGS process
+SCRATCHDIR=/projects/storm_surge/asgs_testing/${INSTANCENAME}
+RMQMessaging_Transmit=on
+QSCRIPTTEMPLATE=$SCRIPTDIR/config/2022/ncfs-dev/qscript.template.renci.ht1
+ADCIRCDIR=/home/bblanton/ADCIRC/v53release.ht1/work
+SWANDIR=/home/bblanton/ADCIRC/v53release.ht1/swan
+#RESERVATION=test
+
+# Input files and templates
+
+GRIDNAME=ec95d
+source $SCRIPTDIR/config/mesh_defaults.sh
+
+# Initial state (overridden by STATEFILE after ASGS gets going)
+
+COLDSTARTDATE=2022081500  # calendar year month day hour YYYYMMDDHH24
+HOTORCOLD=coldstart       # "hotstart" or "coldstart"
+LASTSUBDIR=null
+
+# Physical forcing (defaults set in config/forcing_defaults.sh)
+
+TIDEFAC=on               # tide factor recalc
+   HINDCASTLENGTH=16.0   # length of initial hindcast, from cold (days)
+BACKGROUNDMET=off         # NAM download/forcing
+   FORECASTCYCLE="00,06,12,18"
+TROPICALCYCLONE=on      # tropical cyclone forcing
+   STORM=05              # storm number, e.g. 05=ernesto in 2006
+   YEAR=2022             # year of the storm
+WAVES=on                 # wave forcing
+   REINITIALIZESWAN=no   # used to bounce the wave solution
+VARFLUX=off              # variable river flux forcing
+   RIVERSITE=data.disaster.renci.org
+   RIVERDIR=/opt/ldm/storage/SCOOP/RHLRv9-OKU
+   RIVERUSER=bblanton
+   RIVERDATAPROTOCOL=scp
+CYCLETIMELIMIT="99:00:00"
+
+# Computational Resources (related defaults set in platforms.sh)
+
+NCPU=64                    # number of compute CPUs for all simulations
+NCPUCAPACITY=64
+NUMWRITERS=0
+ACCOUNT=null
+#EXCLUDE="compute-9-xx"
+QUEUENAME="batch"
+
+# Post processing and publication
+
+INTENDEDAUDIENCE=developers-only    # "general" | "developers-only" | "professional"
+
+FINISH_NOWCAST_SCENARIO=( output/opendap_post_nowcast.sh ) # output/run_adda.sh )
+#POSTPROCESS=( accumulateMinMax.sh createMaxCSV.sh cpra_slide_deck_post.sh includeWind10m.sh createOPeNDAPFileList.sh opendap_post.sh )
+POSTPROCESS=( createOPeNDAPFileList.sh opendap_post.sh transmit_rps.sh ncfs_post_to_current_tc.sh )
+#POSTPROCESS=( includeWind10m.sh createOPeNDAPFileList.sh opendap_post.sh transmit_rps.sh )
+
+#OPENDAPNOTIFY="asgs.cera.lsu@gmail.com jason.g.fleming@gmail.com"
+OPENDAPNOTIFY="bblanton@renci.org"
+NOTIFY_SCRIPT=ncfs_nam_notify.sh
+
+# Scenario package
+
+#PERCENT=default
+SCENARIOPACKAGESIZE=1
+case $si in
+   -2) 
+       ENSTORM=hindcast
+       ;;
+   -1)      
+       # do nothing ... this is not a forecast
+       ENSTORM=nowcast
+       ;;
+    0)
+       ENSTORM=nhcOfcl
+       ;;
+    1)
+       ENSTORM=veerLeft100
+       PERCENT=-100
+       ;;
+    2)
+       ENSTORM=veerRight100
+       PERCENT=100
+       ;;
+    *)   
+       echo "CONFIGRATION ERROR: Unknown ensemble member number: '$si'."
+      ;;
+esac
+
+PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz
+HINDCASTARCHIVE=prepped_${GRIDNAME}_hc_${INSTANCENAME}_${NCPU}.tar.gz
