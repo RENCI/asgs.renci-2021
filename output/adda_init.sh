bd50bda10ac0bc6be7b9f203d6005ffd20205bdb
Brian Blanton <bblanton@renci.org>
2022-09-14 12:09:38 -0400

    checking in psc configs

---
+++ output/adda_init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+
+export ADDAHOME="/jet/home/bblanton/adda/"
+export ADDAStorage="$ADDAHOME/data/"
+export PYTHONPATH="/jet/home/bblanton/adda/"
+export RUNTIMEDIR="./adda"
+export PYTHON="/jet/home/bblanton/miniconda3/bin/python"
+
+
+#Dest="$ADDAStorage/current_"$gridname"_dwlc"
+#DestName="$Dest/dwlc_$gridname_$adv.csv"
+#DestLinkName="$Dest/dwlc_$gridname.csv"
+
+#echo "gridname=$gridname"
+#echo "adv=$adv"
+#echo "Dest=$Dest"
+#echo "DestName=$DestName"
+#echo "DestLinkName=$DestLinkName"
+
